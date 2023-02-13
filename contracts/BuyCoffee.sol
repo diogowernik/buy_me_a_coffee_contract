78fef172cd1edd1a3bb6dc5134c1296051ba3e94
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2023-02-13 13:57:13 -0300

    new start with authors and cofee

--- contracts/BuyCoffee.sol
+++ contracts/BuyCoffee.sol
@@ -22,18 +22,20 @@ contract BuyCoffee {
     mapping (address => Author) public authors;
     uint256 public total_contract_balance;
 
-    function author_withdraw(address wallet_address) public {
-        uint256 balance = authors[wallet_address].balance;
-        require(balance > 0, "Author has no balance to withdraw");
-        authors[wallet_address].balance = 0;
-        total_contract_balance -= balance;
-        // address payable wallet_address_payable = address(wallet_address);
-        // wallet_address_payable.transfer(balance);
-        // Type address is not implicitly convertible to expected type address payable.
-
+    function getContractBalance() public view returns (uint256) {
+        return total_contract_balance;
     }
 
-    function new_buymeacoffee(address author_wallet_address, address sender_wallet_address, uint256 amount, uint256 timestamp, string memory message) public {
+    function author_withdraw(address author_wallet_address) public {
+        address payable wallet_address_payable = payable(author_wallet_address);
+        uint256 balance = authors[author_wallet_address].balance;
+        require(balance > 0, "Author has no balance to withdraw");
+        authors[author_wallet_address].balance = 0;
+        total_contract_balance -= balance;
+        wallet_address_payable.transfer(balance);
+    }
+
+    function buymeacoffee(address author_wallet_address, address sender_wallet_address, uint256 amount, uint256 timestamp, string memory message) public {
         coffee.push(Coffee({
             author_wallet_address: author_wallet_address,
             sender_wallet_address: sender_wallet_address,
